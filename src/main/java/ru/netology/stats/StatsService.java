9ec268a0189076c103913aee671c69e189f742c6
Evgenii Semenov <eo.semenoff@gmail.com>
2021-05-25 16:58:05 +0300

    StatsService and tests added

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,68 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int minSales(int[] sales) {  //Определение месяца минимальных продаж
+        int minMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+
+    }
+
+
+    public int maxSales(int[] sales) {  //Определение месяца максимальных продаж
+        int maxMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int sumSales(int[] sales) {    //Определение суммы продаж за все месяцы
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int avgSales(int[] sales) {   //Определение среднеарифметической суммы продаж в месяц
+        int avgSales = sumSales(sales) / 12;
+        return avgSales;
+
+    }
+
+    public int monthsBelowAvgSales(int[] sales) {  //Определение количества месяцев с продажами ниже среднеарифметической в месяц
+        int belowAvgMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale < avgSales(sales)) {
+                belowAvgMonth += 1;
+            }
+            month = month + 1;
+        }
+        return belowAvgMonth;
+    }
+
+    public int monthsAboveAvgSales(int[] sales) {   //Определение количества месяцев с продажами выше среднеарифметической в месяц
+        int aboveAvgMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale > avgSales(sales)) {
+                aboveAvgMonth += 1;
+            }
+            month = month + 1;
+        }
+        return aboveAvgMonth;
+    }
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,60 @@
+package ru.netology.stats;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class StatsServiceTest {
+
+    int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Массив тестовых данных с продажами по месяцам
+
+    @Test
+    void shouldCalculateMinSalesMonth() {
+        StatsService service = new StatsService();
+        int expected = 9;
+        int actual = service.minSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMaxSalesMonth() {
+        StatsService service = new StatsService();
+        int expected = 8;
+        int actual = service.maxSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateSumSales() {
+        StatsService service = new StatsService();
+        int expected = 180;
+        int actual = service.sumSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAverageSales() {
+        StatsService service = new StatsService();
+        int expected = 15;
+        int actual = service.avgSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMonthsBelowAverageSales() {
+        StatsService service = new StatsService();
+        int expected = 5;
+        int actual = service.monthsBelowAvgSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMonthsAboveAverageSales() {
+        StatsService service = new StatsService();
+        int expected = 5;
+        int actual = service.monthsAboveAvgSales(sales);
+        assertEquals(expected, actual);
+    }
+}
